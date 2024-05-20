63e363d39a39fc82ef5e88acce157852554bfad7
AimoeSilver <3300184206@qq.com>
2024-05-20 22:17:42 +0800

    create dev branch

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+export default createStore({
+//   state: {
+//     isLogin: false,
+//     user: {},
+//   },
+//   mutations: {
+//     setLogin(state, payload) {
+//       state.isLogin = payload;
+//     },
+//     setUser(state, payload) {
+//       state.user = payload;
+//     },
+//   },
+//   actions: {},
+//   modules: {},
+})
